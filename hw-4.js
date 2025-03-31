522708f003858351eb0771be5afdc7e15ee5e0d3
Antonova-1988 <nizamytdenova@list.ru>
2025-03-31 15:42:09 +0500

    Добавила мобильную версию и выполнила задания js.

---
+++ hw-4.js
@@ -0,0 +1,67 @@
+// задание 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// задание 2
+const dateIphone = 2007;
+alert(dateIphone);
+
+//задание 3
+const nameCreator = "Brendan Eich";
+alert(nameCreator);
+
+// задание 4
+let c = 10;
+let b = 2;
+alert(c+b);
+alert(c-b);
+alert(c*b);
+alert(c/b);
+
+// Задание 5
+let result = 2**5;
+alert(result);
+
+// Задание 6
+let d = 9;
+let i = 2;
+alert(d%i);
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+// Задание 8
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+//Задание 9
+const user = {
+    name:'Иван',
+    age: 21,
+    isAdmin: true,
+}
+
+// Задание 10
+let userName = prompt("Введите Ваше имя");
+alert(`Привет, ${userName}!`);
+
+//Дополнительное задание
+let number = prompt("Загадайте любое число");
+let product = number * 2;
+alert(product);
+let sum = product + 10;
+alert(sum);
+let quotient = sum / 2;
+alert(quotient);
+let difference = quotient - 5;
+alert(`Ответ равен ${difference}!`);
+
